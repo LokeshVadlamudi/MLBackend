c06676024762035d6066547f0349347d76f83406
Lokesh Vadlamudi <lokesh.dev96@gmail.com>
2019-11-23 23:53:28 -0800

    compressed

--- Recommend.py
+++ Recommend.py
@@ -1,14 +1,30 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from flask_swagger_ui import get_swaggerui_blueprint
+import json
+import pymongo
+
 from pymongo import MongoClient
 from flask import jsonify
 import pandas as pd
 from sklearn.neighbors import NearestNeighbors
 from scipy.sparse import csr_matrix
 
+app = Flask(__name__)
+
+# Supporting Cross Origin requests for all APIs
+cors = CORS(app)
+
 conf = {
     "mongo_url" : "mongodb+srv://lokv007:lokesh99@mongodb-2fhcm.mongodb.net/admin?ssl=true&ssl_cert_reqs=CERT_NONE",
     #"host_url": "http://lmp.nupursjsu.net"
     "host": "http://localhost"
 }
+
+@app.route('/v1/books/<string:Book_id>/recommendations', methods=['GET'])
+def get_recommended_books(Book_id):
+    response = recommend_books(Book_id)
+    return response, 200
 
 def recommend_books(book_id):
 
@@ -37,3 +53,7 @@ def recommend_books(book_id):
 		for i in get:
 			output.append(i)
 	return jsonify({'result' : output})
+
+if __name__ == '__main__':
+    app.run(host='18.218.64.73',port=8080)
+    # app.run(host=conf['host'], port=conf['port'], debug=True)
